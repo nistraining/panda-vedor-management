0332bd95203e9021bfee5423ffa6251d048754f0
Nisith <nisith.bpt@gmail.com>
2025-07-05 15:15:14 +0200

    Integrated with Kafka avro schema registry to produce the response from the respective vendor

--- src/main/java/panda/vendor/management/services/OrderValidationConsumer.java
+++ src/main/java/panda/vendor/management/services/OrderValidationConsumer.java
@@ -4,23 +4,28 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.panda.vendor.management.entities.VendorResponseDTO;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import panda.vendor.management.dto.VendorOrderRequest;
-import panda.vendor.management.dto.VendorResponseDTO;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @Service
 public class OrderValidationConsumer {
-
+	
 	@Autowired
-	private SqsTemplate sqsTemplate;
+	private KafkaTemplate<String, VendorResponseDTO> kafkaTemplate;
+	
+	@Value("${topic.name}")
+	private String topicName;
 
 	@SqsListener("https://sqs.eu-central-1.amazonaws.com/489855987447/panda-foods-queue")
 	public void processOrderRequest(@Payload VendorOrderRequest request) {
@@ -39,21 +44,28 @@ public class OrderValidationConsumer {
 	}
 
 	public void respondToOrderService(VendorOrderRequest request, boolean isDeliverable) {
-		VendorResponseDTO vendorOrderResponseDTO = new VendorResponseDTO();
+		VendorResponseDTO vendorOrderResponseDTO =new VendorResponseDTO();
 		vendorOrderResponseDTO.setOrderName(request.getOrderName());
 		vendorOrderResponseDTO.setQuantity(request.getQuantity());
 		vendorOrderResponseDTO.setOrderLocation(request.getOrderLocation());
 		vendorOrderResponseDTO.setDeliverable(isDeliverable);
 		vendorOrderResponseDTO.setMessageType("RESPONSE");
+//		try {
+//			sqsTemplate.send(
+//					"https://sqs.eu-central-1.amazonaws.com/489855987447/nisith-tech-queue",
+//					vendorOrderResponseDTO
+//					);
+//
+//
+//		} catch (Exception e) {
+//			System.err.println("Serialization or send error: " + e.getMessage());
+//		}
+		
 		try {
-			sqsTemplate.send(
-					"https://sqs.eu-central-1.amazonaws.com/489855987447/nisith-tech-queue",
-					vendorOrderResponseDTO
-					);
-
-
-		} catch (Exception e) {
-			System.err.println("Serialization or send error: " + e.getMessage());
+			kafkaTemplate.send(topicName,vendorOrderResponseDTO);
+			System.out.println("Sent Vendor response to kafka topic");
+		}catch(Exception e) {
+			System.out.println("Kafka publish error :"+ e.getMessage());
 		}
 
 	}
