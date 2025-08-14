ea6d251f3c0f1af539860944b7adf28cdd078ce7
Nisith <nisith.bpt@gmail.com>
2025-08-15 01:15:37 +0200

    Fixed pod statups issues

--- wait-for-kafka.sh
+++ wait-for-kafka.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # Give Docker DNS a moment to settle
 sleep 5
 
-echo "Waiting for Kafka broker at broker:29092..."
-until nc -z broker 29092; do
+echo "Waiting for Kafka broker at kafka-broker-0.kafka-broker.default.svc.cluster.local:9092..."
+until nc -z kafka-broker-0.kafka-broker.default.svc.cluster.local 9092; do
   echo "Kafka broker not yet available..."
   sleep 2
 done
 echo "Kafka broker is up!"
 
-echo "Waiting for Schema Registry at schema-registry:8081..."
-until nc -z schema-registry 8081; do
+echo "Waiting for Schema Registry at schema-registry-service:8081..."
+until nc -z schema-registry-service 8081; do
   echo "Schema Registry not yet available..."
   sleep 2
 done
